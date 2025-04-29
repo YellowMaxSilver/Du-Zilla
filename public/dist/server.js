75535b5bb1af533fb2a7c28d5c013031507126c4
Eduardo Sodré <90865245+YellowMaxSilver@users.noreply.github.com>
2025-04-29 16:10:41 -0300

    home page upgreat

--- public/dist/server.js
+++ public/dist/server.js
@@ -4,19 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(express_1.default.static(path_1.default.join(__dirname, "./public")));
 app.get('/:page', (req, res) => {
     const urlPage = req.params.page;
     if (urlPage != null) {
-        res.send(`Welcome to the port ${port}. page ${urlPage}`);
+        res.sendFile(path_1.default.join(__dirname, `./public/${urlPage}.html`), (err) => {
+            if (err) {
+                res.status(404).send(`page not found`);
+            }
+        });
     }
     else {
         res.send(`Welcome to the port ${port}`);
     }
 });
 app.get('/', (req, res) => {
-    res.send(`Welcome to the port ${port}`);
+    res.sendFile(path_1.default.join(__dirname, './public/index.html'));
 });
 app.listen(port, () => {
     console.log(`server running in the port ${port}`);
--- server.ts
+++ server.ts
@@ -1,19 +1,26 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,"./public")))
+
 app.get('/:page',(req,res)=>{
     const urlPage = req.params.page;
     if(urlPage != null){
-        res.send(`Welcome to the port ${port}. page ${urlPage}`);
+        res.sendFile(path.join(__dirname,`./public/${urlPage}.html`),(err)=>{
+            if(err){
+                res.status(404).send(`page not found`);
+            }
+        });
     }else{
         res.send(`Welcome to the port ${port}`)
     }
 });
 
 app.get('/',(req,res)=>{
-    res.send(`Welcome to the port ${port}`)
+    res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
 app.listen(port,()=>{
