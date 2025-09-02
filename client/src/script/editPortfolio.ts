31c455273da49b4d400a3bbbcf60844050cd5dfc
yellowmaxsilver <90865245+YellowMaxSilver@users.noreply.github.com>
2025-09-02 16:42:00 -0300

    update classes

--- client/src/script/editPortfolio.ts
+++ client/src/script/editPortfolio.ts
@@ -1,35 +1,92 @@
 import compilerToString from "./compiler";
+import { elementRandomId } from "./idGenerete";
 
-let mainView = document.getElementById("preview") as HTMLElement;
 
-class Text{
-    constructor(text:string,
-        size:number,
-        color:string,
-        positionX:number,
-        positionY:number,
-        width:number,
-        height:number){
+const mainView = document.getElementById("createPreviewContent") as HTMLElement;
+var clickedBox = document.getElementById("clickedBox") as HTMLElement;
+var overBox = document.getElementById("selectedBox") as HTMLElement;
 
-        let htmlEdit:string = `<h1 id="123456" style="position:absolute;top:0;left:0;
+class Widget {
+    constructor(
+        widgetType: string,
+        text: string,
+        size: number,
+        color: string,
+        positionX: number,
+        positionY: number,
+        width: number,
+        height: number
+    ) {
+
+        let elementId: string = elementRandomId();
+        let active: boolean = false;
+
+        let htmlEdit: string = `<${widgetNameToHtmlTag(widgetType)} id="${elementId}" 
+        style="position:absolute;top:0;left:0;
         margin-left:${positionX}px;
         margin-top:${positionY}px;
-        font-size:${size}px;">${text}</h1>`;
-        mainView.innerHTML += htmlEdit;
+        font-size:${size}px;
+        width:${width}px;
+        height:${height}px;
+        color:${color};
+        ">${text}</${widgetNameToHtmlTag(widgetType)}>`;
 
-        const element = document.getElementById("123456") as HTMLElement;
+        mainView.insertAdjacentHTML('beforeend', htmlEdit);
 
-        element.addEventListener("mouseover",()=>{
-            element.style = "border: 2px solid green";
-        })
+        let element = mainView.querySelector(`#${elementId}`) as HTMLElement;
+
+        if (element) {
+            element.addEventListener("click", () => {
+                onWidgetClicked(elementId, positionX, positionY, width, height);
+                active = true;
+            })
+
+            element.addEventListener("mouseenter", () => {
+                onWidgetMouseOver(positionX, positionY, width, height);
+            })
+        }
+
+        function refresh(widgetId: string) {
+            let widgetFound = mainView.querySelector(`#${widgetId}`) as HTMLElement;
+            if (widgetFound) widgetFound.remove();
+            mainView.insertAdjacentHTML('beforeend', htmlEdit);
+        }
     }
+
+}
+
+function onWidgetMouseOver(positionX: number, positionY: number, width: number, height: number) {
+    overBox.style = `display:flex;margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
+}
+
+function onWidgetClicked(widgetId: string, positionX: number, positionY: number, width: number, height: number) {
+    let widget: HTMLElement | undefined = document.getElementById(widgetId) as HTMLElement;
+    clickedBox.style = `margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
 }
 
 //{!}{text(element)}{text}{size}{color}{positionX}{positionY}{width}{height}{/!}
 
-const mainTemplate:string = "{!}{text}{hello World}{30}{#00000}{100}{200}{100}{100}{/!}{!}{text}{hello World again}{30}{#00000}{100}{200}{100}{100}{/!}";
+const mainTemplate: string = "{!}{text}{hello World}{30}{#00000}{100}{200}{100}{100}{/!}{!}{text}{hello World again}{30}{#00000}{100}{200}{100}{100}{/!}";
 
-var widgets:string[][] = compilerToString(mainTemplate);
+var widgets: string[][] = compilerToString(mainTemplate);
 
-new Text("hello",52,"#ffff",100,120,120,120);
-console.log(widgets)
+new Widget("text","hello", 52, "#00000", 0, 0, 120, 120);
+new Widget("text","hello again", 52, "#00000", 300, 120, 120, 120);
+new Widget("text","hello again", 32, "#00000", 190, 160, 220, 120);
+console.log(widgets);
+
+// .faca{color:red;
+// position:absolute;
+// top:0;
+// left:0;margin-left:100px;margin-top:100px;width:100px;height:100px;}
+
+function widgetNameToHtmlTag(widgetName: string): string {
+    switch (widgetName) {
+        case "text":
+            return "h1";
+            break;
+        default:
+            return "div";
+            break;
+    }
+}
--- dist/client/src/script/editPortfolio.js
+++ dist/client/src/script/editPortfolio.js
@@ -4,22 +4,67 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const compiler_1 = __importDefault(require("./compiler"));
-let mainView = document.getElementById("preview");
-class Text {
-    constructor(text, size, color, positionX, positionY, width, height) {
-        let htmlEdit = `<h1 id="123456" style="position:absolute;top:0;left:0;
+const idGenerete_1 = require("./idGenerete");
+const mainView = document.getElementById("createPreviewContent");
+var clickedBox = document.getElementById("clickedBox");
+var overBox = document.getElementById("selectedBox");
+class Widget {
+    constructor(widgetType, text, size, color, positionX, positionY, width, height) {
+        let elementId = (0, idGenerete_1.elementRandomId)();
+        let active = false;
+        let htmlEdit = `<${widgetNameToHtmlTag(widgetType)} id="${elementId}" 
+        style="position:absolute;top:0;left:0;
         margin-left:${positionX}px;
         margin-top:${positionY}px;
-        font-size:${size}px;">${text}</h1>`;
-        mainView.innerHTML += htmlEdit;
-        const element = document.getElementById("123456");
-        element.addEventListener("mouseover", () => {
-            element.style = "border: 2px solid green";
-        });
+        font-size:${size}px;
+        width:${width}px;
+        height:${height}px;
+        color:${color};
+        ">${text}</${widgetNameToHtmlTag(widgetType)}>`;
+        mainView.insertAdjacentHTML('beforeend', htmlEdit);
+        let element = mainView.querySelector(`#${elementId}`);
+        if (element) {
+            element.addEventListener("click", () => {
+                onWidgetClicked(elementId, positionX, positionY, width, height);
+                active = true;
+            });
+            element.addEventListener("mouseenter", () => {
+                onWidgetMouseOver(positionX, positionY, width, height);
+            });
+        }
+        function refresh(widgetId) {
+            let widgetFound = mainView.querySelector(`#${widgetId}`);
+            if (widgetFound)
+                widgetFound.remove();
+            mainView.insertAdjacentHTML('beforeend', htmlEdit);
+        }
     }
+}
+function onWidgetMouseOver(positionX, positionY, width, height) {
+    overBox.style = `display:flex;margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
+}
+function onWidgetClicked(widgetId, positionX, positionY, width, height) {
+    let widget = document.getElementById(widgetId);
+    clickedBox.style = `margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
 }
 //{!}{text(element)}{text}{size}{color}{positionX}{positionY}{width}{height}{/!}
 const mainTemplate = "{!}{text}{hello World}{30}{#00000}{100}{200}{100}{100}{/!}{!}{text}{hello World again}{30}{#00000}{100}{200}{100}{100}{/!}";
 var widgets = (0, compiler_1.default)(mainTemplate);
-new Text("hello", 52, "#ffff", 100, 120, 120, 120);
+new Widget("text", "hello", 52, "#00000", 0, 0, 120, 120);
+new Widget("text", "hello again", 52, "#00000", 300, 120, 120, 120);
+new Widget("text", "hello again", 32, "#00000", 190, 160, 220, 120);
 console.log(widgets);
+// .faca{color:red;
+// position:absolute;
+// top:0;
+// left:0;margin-left:100px;margin-top:100px;width:100px;height:100px;}
+function widgetNameToHtmlTag(widgetName) {
+    switch (widgetName) {
+        case "text":
+            return "h1";
+            break;
+        default:
+            return "div";
+            break;
+    }
+}
