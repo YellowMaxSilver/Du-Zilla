4b629d14f484b8bdcfd133039691cf47cf1da3eb
Eduardo Sodré <90865245+YellowMaxSilver@users.noreply.github.com>
2025-09-19 14:55:29 -0300

    real-time-change-update

--- client/src/script/editPortfolio.ts
+++ client/src/script/editPortfolio.ts
@@ -6,6 +6,15 @@ const mainView = document.getElementById("createPreviewContent") as HTMLElement;
 var clickedBox = document.getElementById("clickedBox") as HTMLElement;
 var overBox = document.getElementById("selectedBox") as HTMLElement;
 
+const attributesPanel = document.querySelector('#attributesPanel') as HTMLElement;
+const attributesPanelTextInput = attributesPanel.querySelector('#textInput') as HTMLInputElement;
+const attributesPanelCloseButton = document.querySelector("#attributesPanelCloseButton") as HTMLElement;
+var widgetIdOfAttributePanel:string|undefined = undefined;
+
+attributesPanelCloseButton.addEventListener('click',()=>{
+    attributesPanel.style.display = "none";
+})
+
 class Widget {
     constructor(
         widgetType: string,
@@ -22,13 +31,13 @@ class Widget {
         let active: boolean = false;
 
         let htmlEdit: string = `<${widgetNameToHtmlTag(widgetType)} id="${elementId}" 
-        style="position:absolute;top:0;left:0;
+        style="
         margin-left:${positionX}px;
         margin-top:${positionY}px;
         font-size:${size}px;
-        width:${width}px;
-        height:${height}px;
         color:${color};
+        width:auto;
+        height:auto;
         ">${text}</${widgetNameToHtmlTag(widgetType)}>`;
 
         mainView.insertAdjacentHTML('beforeend', htmlEdit);
@@ -37,12 +46,12 @@ class Widget {
 
         if (element) {
             element.addEventListener("click", () => {
-                onWidgetClicked(elementId, positionX, positionY, width, height);
+                onWidgetClicked(elementId);
                 active = true;
             })
 
             element.addEventListener("mouseenter", () => {
-                onWidgetMouseOver(positionX, positionY, width, height);
+                onWidgetMouseOver(elementId);
             })
         }
 
@@ -55,13 +64,46 @@ class Widget {
 
 }
 
-function onWidgetMouseOver(positionX: number, positionY: number, width: number, height: number) {
+function onWidgetMouseOver(widgetId:string) {
+    let widget: HTMLElement | undefined = document.getElementById(widgetId) as HTMLElement;
+    let positionX:number = widget.offsetLeft;
+    let positionY:number = widget.offsetTop;
+    let width:any = widget.offsetWidth;
+    let height:any = widget.offsetHeight;
     overBox.style = `display:flex;margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
 }
 
-function onWidgetClicked(widgetId: string, positionX: number, positionY: number, width: number, height: number) {
+function onWidgetClicked(widgetId: string) {
     let widget: HTMLElement | undefined = document.getElementById(widgetId) as HTMLElement;
+    let positionX:number = widget.offsetLeft;
+    let positionY:number = widget.offsetTop;
+    let width:any = widget.offsetWidth;
+    let height:any = widget.offsetHeight;
     clickedBox.style = `margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
+    attributesPanel.style.display = "block";
+    widgetIdOfAttributePanel = widgetId;
+    setAttributePanel();
+    //console.log(widget.textContent);
+}
+
+function refresh(widgetId:string){
+    let widget: HTMLElement | undefined = document.getElementById(widgetId) as HTMLElement;
+    
+}
+
+function setAttributePanel(){
+    if(widgetIdOfAttributePanel == undefined){
+        return;
+    }
+    let widget: HTMLElement | undefined = document.getElementById(widgetIdOfAttributePanel) as HTMLElement;
+    attributesPanelTextInput.value = widget.textContent;
+
+    setInterval(()=>{
+        if(widgetIdOfAttributePanel == undefined){
+            return;
+        }
+        (document.getElementById(widgetIdOfAttributePanel) as HTMLElement).innerHTML = attributesPanelTextInput.value;
+    },1000)
 }
 
 //{!}{text(element)}{text}{size}{color}{positionX}{positionY}{width}{height}{/!}
@@ -70,9 +112,11 @@ const mainTemplate: string = "{!}{text}{hello World}{30}{#00000}{100}{200}{100}{
 
 var widgets: string[][] = compilerToString(mainTemplate);
 
-new Widget("text","hello", 52, "#00000", 0, 0, 120, 120);
-new Widget("text","hello again", 52, "#00000", 300, 120, 120, 120);
-new Widget("text","hello again", 32, "#00000", 190, 160, 220, 120);
+new Widget("text","Title", 52, "#00000", 40, 40, 100, 100);
+new Widget("text","Subtitle", 52, "#00000", 40, 0, 100, 100);
+new Widget("text","Description", 32, "#00000", 40, 0, 100, 100);
+new Widget("form","form1", 32, "#00000", 40, 0, 100, 100);
+
 console.log(widgets);
 
 // .faca{color:red;
--- dist/client/src/script/editPortfolio.js
+++ dist/client/src/script/editPortfolio.js
@@ -8,28 +8,35 @@ const idGenerete_1 = require("./idGenerete");
 const mainView = document.getElementById("createPreviewContent");
 var clickedBox = document.getElementById("clickedBox");
 var overBox = document.getElementById("selectedBox");
+const attributesPanel = document.querySelector('#attributesPanel');
+const attributesPanelTextInput = attributesPanel.querySelector('#textInput');
+const attributesPanelCloseButton = document.querySelector("#attributesPanelCloseButton");
+var widgetIdOfAttributePanel = undefined;
+attributesPanelCloseButton.addEventListener('click', () => {
+    attributesPanel.style.display = "none";
+});
 class Widget {
     constructor(widgetType, text, size, color, positionX, positionY, width, height) {
         let elementId = (0, idGenerete_1.elementRandomId)();
         let active = false;
         let htmlEdit = `<${widgetNameToHtmlTag(widgetType)} id="${elementId}" 
-        style="position:absolute;top:0;left:0;
+        style="
         margin-left:${positionX}px;
         margin-top:${positionY}px;
         font-size:${size}px;
-        width:${width}px;
-        height:${height}px;
         color:${color};
+        width:auto;
+        height:auto;
         ">${text}</${widgetNameToHtmlTag(widgetType)}>`;
         mainView.insertAdjacentHTML('beforeend', htmlEdit);
         let element = mainView.querySelector(`#${elementId}`);
         if (element) {
             element.addEventListener("click", () => {
-                onWidgetClicked(elementId, positionX, positionY, width, height);
+                onWidgetClicked(elementId);
                 active = true;
             });
             element.addEventListener("mouseenter", () => {
-                onWidgetMouseOver(positionX, positionY, width, height);
+                onWidgetMouseOver(elementId);
             });
         }
         function refresh(widgetId) {
@@ -40,19 +47,49 @@ class Widget {
         }
     }
 }
-function onWidgetMouseOver(positionX, positionY, width, height) {
+function onWidgetMouseOver(widgetId) {
+    let widget = document.getElementById(widgetId);
+    let positionX = widget.offsetLeft;
+    let positionY = widget.offsetTop;
+    let width = widget.offsetWidth;
+    let height = widget.offsetHeight;
     overBox.style = `display:flex;margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
 }
-function onWidgetClicked(widgetId, positionX, positionY, width, height) {
+function onWidgetClicked(widgetId) {
     let widget = document.getElementById(widgetId);
+    let positionX = widget.offsetLeft;
+    let positionY = widget.offsetTop;
+    let width = widget.offsetWidth;
+    let height = widget.offsetHeight;
     clickedBox.style = `margin-left:${positionX}px;margin-top:${positionY}px;width:${width}px;height:${height}px;`;
+    attributesPanel.style.display = "block";
+    widgetIdOfAttributePanel = widgetId;
+    setAttributePanel();
+    //console.log(widget.textContent);
+}
+function refresh(widgetId) {
+    let widget = document.getElementById(widgetId);
+}
+function setAttributePanel() {
+    if (widgetIdOfAttributePanel == undefined) {
+        return;
+    }
+    let widget = document.getElementById(widgetIdOfAttributePanel);
+    attributesPanelTextInput.value = widget.textContent;
+    setInterval(() => {
+        if (widgetIdOfAttributePanel == undefined) {
+            return;
+        }
+        document.getElementById(widgetIdOfAttributePanel).innerHTML = attributesPanelTextInput.value;
+    }, 1000);
 }
 //{!}{text(element)}{text}{size}{color}{positionX}{positionY}{width}{height}{/!}
 const mainTemplate = "{!}{text}{hello World}{30}{#00000}{100}{200}{100}{100}{/!}{!}{text}{hello World again}{30}{#00000}{100}{200}{100}{100}{/!}";
 var widgets = (0, compiler_1.default)(mainTemplate);
-new Widget("text", "hello", 52, "#00000", 0, 0, 120, 120);
-new Widget("text", "hello again", 52, "#00000", 300, 120, 120, 120);
-new Widget("text", "hello again", 32, "#00000", 190, 160, 220, 120);
+new Widget("text", "Title", 52, "#00000", 40, 40, 100, 100);
+new Widget("text", "Subtitle", 52, "#00000", 40, 0, 100, 100);
+new Widget("text", "Description", 32, "#00000", 40, 0, 100, 100);
+new Widget("form", "form1", 32, "#00000", 40, 0, 100, 100);
 console.log(widgets);
 // .faca{color:red;
 // position:absolute;
